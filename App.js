3c35ba263cf129645e2c92d3fe8c92b5e463b647
Nguyễn Trọng Khang <79756284+trongkhang020801@users.noreply.github.com>
2024-05-26 11:19:31 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator, CardStyleInterpolators } from "@react-navigation/stack";
